4d66898018e403291d529e13f58e9723a537650d
Beverly Tan <43435349+beverlytan@users.noreply.github.com>
2018-11-02 10:09:15 +0000

    Created jackfruit tacos recipe

---
+++ veg/jackfruit-tacos.m
@@ -0,0 +1,55 @@
+Serves 4!
+
+Ingredients for jackfruit
+- Two 560ml cans of jackfruit in water or brine (NOT syrup) 
+- 1/2 large white or yellow onion, thinly sliced
+- 4 cloves garlic, minced
+- 2 tbsp vegetable oil 
+- 1/2 tsp sea salt
+- 1 tbsp ground smoked paprika 
+- 1 tbsp ground cumin 
+- 1 tbsp chili powder (lessed for less spicy) 
+- 2-3 tbsp brown sugar 
+- 1 tbsp adobo sauce 
+  - 1 tbsp tomato paste / ketchup 
+  - 1 tbsp cider vinegar 
+  - 1 tsp smoked paprika/cayenne powder mix 
+  - 1/2 tsp cumin 
+  - Pinch of oregano 
+  - Pinch of salt
+- 1/4 cup soy sauce 
+- 2/3 cup water 
+- 3 tbsp lime juice 
+
+Instructions for jackfruit 
+- Thoroughly rinse and drain two 560ml cans of jackfruit in water/brine
+- The pieces come in chunks or triangle shapes
+- Cut off the parts of the jackfruit that is toughter in texture, and separate it from the parts that appear more stringy 
+  - Chop the tougher texture parts into smaller pieces
+  - Use your hands to pull the stringier parts into small, shredded piecces 
+- Rinse the jackfruit once more in a colander and drain
+- Slice 1/2 large white or yellow onion into thin slices
+- Mince 4 cloves of garlic 
+- Heat a large pan over medium heat, and when hot, add 2 tbsp oil 
+- Add onion and garlic and saute for 4-5 minutes, or until onions are transparent and softened
+- Add jackfruit, and the following ingredients to the pan. Stir to coat and lower heat to low-medium. Cover and cook, stirring occasionally so that it does not burn 
+  - 1/2 tsp sea salt
+  - 1 tbsp ground smoked paprika 
+  - 1 tbsp ground cumin 
+  - 1 tbsp chili powder (lessed for less spicy) 
+  - 2-3 tbsp brown sugar 
+  - 1 tbsp adobo sauce 
+    - 1 tbsp tomato paste / ketchup 
+    - 1 tbsp cider vinegar 
+    - 1 tsp smoked paprika/cayenne powder mix 
+    - 1/2 tsp cumin 
+    - Pinch of oregano 
+    - Pinch of salt
+  - 1/4 cup soy sauce 
+  - 2/3 cup water 
+  - 3 tbsp lime juice 
+- For finer texture, as your jackfruit is cooking, use your spoon to mash the jackfruit into smaller pieces or use two forks to shred the jackfruit as it cooks down
+- Enjoy on top of salad / nachos / in burritos / tacos! 
+
+Recipe taken from [Minimalist Baker](https://minimalistbaker.com/spicy-jackfruit-tacos-1-pot-meal/)
+
